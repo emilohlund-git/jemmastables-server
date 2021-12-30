7ec2aa0070e3459335c0cee6cb8758ef2c42e5aa
Emil ÃÖlund <emil.olund@jayway.com>
2021-12-30 16:20:36 +0100

    Added Vercel app to Cors origin list

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ const main = async () => {
         'http://localhost:3000',
         'https://studio.apollographql.com',
         'http://localhost:4000/graphql',
+        'https://jemmastables.vercel.app/',
       ],
       credentials: true,
     }),
